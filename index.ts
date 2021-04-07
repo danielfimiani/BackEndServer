21538ab9cbe4f9b721658d71aaea88c85535bb2e
Daniel Fimiani <danielfimiani@outlook.com>
2021-04-07 19:56:31 -0300

    Arreglo ramas

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Hola Mundo escuchando");
+import Server from './class/server';
+
+const server = new Server();
+
+server.start(() => {
+    console.log(`Servidor corriendo en ${server.host}:${server.port}`);
+});
