2eba50fa18e9e45d6c26b86310e3ad6bcf8b2658
Daniel Fimiani <danielfimiani@outlook.com>
2021-04-07 20:33:43 -0300

    Primer commit a dev

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log("Hola Mundo escuchando");
+import Server from './class/server';
+
+const server = new Server();
+server.start(() => {
+    console.log(`Server running on ${server.host}:${server.port}`);
+});
